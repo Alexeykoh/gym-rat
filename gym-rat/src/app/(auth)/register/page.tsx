dd864b32c6a40820d5b7b293cd09caed7172d31c
Alexeykoh <Alexeykoh@icloud.com>
2024-01-24 17:59:11 +0300

    feat: create registration page

---
+++ gym-rat/src/app/(auth)/register/page.tsx
@@ -0,0 +1,9 @@
+import RegistrationForm from "@/components/forms/register/register";
+
+export default function Login() {
+  return (
+    <div className="w-full h-full flex flex-col gap-10 items-center justify-center">
+      <RegistrationForm />
+    </div>
+  );
+}
