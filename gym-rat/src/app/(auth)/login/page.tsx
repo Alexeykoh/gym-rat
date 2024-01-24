719a6af14b6b28f9d83443c740480861ed01447b
Alexeykoh <Alexeykoh@icloud.com>
2024-01-24 17:58:52 +0300

    feat: create login component

---
+++ gym-rat/src/app/(auth)/login/page.tsx
@@ -0,0 +1,9 @@
+import LoginForm from "@/components/forms/login/login";
+
+export default function Login() {
+  return (
+    <div className="w-full h-full flex flex-col gap-10 items-center justify-center">
+      <LoginForm />
+    </div>
+  );
+}
