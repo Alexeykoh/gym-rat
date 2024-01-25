ace72f8513a44604e23fc7d24e4f6477a7a3b897
Alexeykoh <alexeykoh@gmail.com>
2024-01-26 00:50:24 +0300

    feat: add sign-up link

--- gym-rat/src/app/(auth)/sign-in/page.tsx
+++ gym-rat/src/app/(auth)/sign-in/page.tsx
@@ -1,9 +1,13 @@
 import LoginForm from "@/components/forms/login/login";
+import Link from "next/link";
 
 export default function Login() {
   return (
     <div className="w-full h-full flex flex-col gap-10 items-center justify-center">
       <LoginForm />
+      <Link className="text-gray-500 hover:text-gray-400 duration-150" href={"/sign-up"}>
+        Create a new account? <span className="font-semibold">Sign up</span>
+      </Link>
     </div>
   );
 }
