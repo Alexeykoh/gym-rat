f9e4c714c6be340fee3ebb8550df3957381da028
Alexeykoh <Alexeykoh@icloud.com>
2024-01-25 22:11:53 +0300

    refactor: update mongo connection

--- gym-rat/src/lib/mongodb.ts
+++ gym-rat/src/lib/mongodb.ts
@@ -2,7 +2,13 @@ import mongoose from "mongoose";
 
 const connectMongoDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO as string);
+    await mongoose.connect(
+      process.env.MONGO as string,
+      {
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+      } as any
+    );
     console.log("Connected to MongoDB");
   } catch (error) {
     console.log(error);
