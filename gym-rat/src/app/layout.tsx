79a4f7574c44c40322ee9cb85000467b4de58997
Alexeykoh <Alexeykoh@icloud.com>
2024-01-25 22:06:59 +0300

    feat: add auth provider

--- gym-rat/src/app/layout.tsx
+++ gym-rat/src/app/layout.tsx
@@ -1,3 +1,4 @@
+import AuthProvider from "@/components/AuthProvider/AuthProvider";
 import Header from "@/components/header/header";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
@@ -16,18 +17,20 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body
-        className={
-          (inter.className,
-          " flex flex-col mx-auto container text-white box-border bg-zinc-800")
-        }
-      >
-        <Header />
-        <main className="pt-20 flex shrink w-full max-h-full box-border p-4">
-          {children}
-        </main>
-      </body>
-    </html>
+    <AuthProvider>
+      <html lang="en">
+        <body
+          className={
+            (inter.className,
+            " flex flex-col mx-auto container text-white box-border bg-zinc-800")
+          }
+        >
+          <Header />
+          <main className="pt-20 flex shrink w-full max-h-full box-border p-4">
+            {children}
+          </main>
+        </body>
+      </html>
+    </AuthProvider>
   );
 }
