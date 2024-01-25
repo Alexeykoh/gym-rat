b3156c07c5b9336000dbd51849e866b969710811
Alexeykoh <Alexeykoh@icloud.com>
2024-01-25 22:07:52 +0300

    refactor: remove useless files

--- gym-rat/src/app/(auth)/register/page.tsx
+++
@@ -1,9 +0,0 @@
-import RegistrationForm from "@/components/forms/register/register";
-
-export default function Login() {
-  return (
-    <div className="w-full h-full flex flex-col gap-10 items-center justify-center">
-      <RegistrationForm />
-    </div>
-  );
-}
--- gym-rat/src/app/account/page.tsx
+++
@@ -1,9 +0,0 @@
-import { FC } from "react";
-
-type pageProps = {};
-
-const Account: FC<pageProps> = () => {
-  return <>this is account page</>;
-};
-
-export default Account;
