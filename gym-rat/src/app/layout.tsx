42e8181ee4c185c3015971c70aa9dcbff788d4b3
Alexeykoh <Alexeykoh@icloud.com>
2024-01-26 13:35:56 +0300

    feat: update layout

--- gym-rat/src/app/layout.tsx
+++ gym-rat/src/app/layout.tsx
@@ -1,5 +1,4 @@
 import AuthProvider from "@/components/AuthProvider/AuthProvider";
-import Header from "@/components/header/header";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
@@ -19,16 +18,8 @@ export default function RootLayout({
   return (
     <AuthProvider>
       <html lang="en">
-        <body
-          className={
-            (inter.className,
-            " flex flex-col mx-auto container text-white box-border bg-zinc-800")
-          }
-        >
-          <Header />
-          <main className="pt-20 flex shrink w-full max-h-full box-border p-4">
-            {children}
-          </main>
+        <body className={(inter.className, "text-white bg-zinc-800")}>
+          {children}
         </body>
       </html>
     </AuthProvider>
