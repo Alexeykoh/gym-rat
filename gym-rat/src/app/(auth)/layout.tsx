c2c0551051fa840d5322f57443bbea63de495ddc
Alexeykoh <Alexeykoh@icloud.com>
2024-01-26 15:12:53 +0300

    refactor: remove useless session check

--- gym-rat/src/app/(auth)/layout.tsx
+++ gym-rat/src/app/(auth)/layout.tsx
@@ -1,13 +1,10 @@
-import { getServerSession } from "next-auth";
 import { ReactNode } from "react";
-import { authOption } from "../api/auth/[...nextauth]/route";
 
 type layoutProps = {
   children: ReactNode;
 };
 
 export default async function AuthLayout({ children }: layoutProps) {
-  const session = await getServerSession(authOption);
   //
   return (
     <main className="w-screen h-screen flex flex-col items-center justify-center">
