6e9b1f7a555dbefcc3b72f2c8bb0a96081e961b6
Alexeykoh <Alexeykoh@icloud.com>
2024-01-26 15:13:15 +0300

    fix: update session check

--- gym-rat/src/app/(private_route)/layout.tsx
+++ gym-rat/src/app/(private_route)/layout.tsx
@@ -1,18 +1,19 @@
+"use client";
 import Header from "@/components/header/header";
-import { getServerSession } from "next-auth";
+import { useSession } from "next-auth/react";
 import { redirect } from "next/navigation";
 import { ReactNode } from "react";
-import { authOption } from "../api/auth/[...nextauth]/route";
 
 type layoutProps = {
   children: ReactNode;
 };
 
-export default async function PrivateLayout({ children }: layoutProps) {
-  const session = await getServerSession(authOption);
-  if (!session?.user) {
+export default function PrivateLayout({ children }: layoutProps) {
+  const { data: session, status } = useSession();
+  if (status !== "authenticated") {
     redirect("/sign-in");
   }
+
   return (
     <>
       <main className="container mx-auto flex flex-col w-full">
