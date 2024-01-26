724867719aacec305e92d3f3a82572efabeeda1f
Alexeykoh <Alexeykoh@icloud.com>
2024-01-26 13:48:01 +0300

    fix: create check db connection

--- gym-rat/src/lib/mongodb.ts
+++ gym-rat/src/lib/mongodb.ts
@@ -1,17 +1,27 @@
 import mongoose from "mongoose";
 
 const connectMongoDB = async () => {
-  try {
-    await mongoose.connect(
-      process.env.MONGO as string,
-      {
-        useUnifiedTopology: true,
-        useNewUrlParser: true,
-      } as any
-    );
-    console.log("Connected to MongoDB");
-  } catch (error) {
-    console.log(error);
+  //
+  if (mongoose.connection.readyState === 1) {
+    console.log("Mongoose is already connected to the database");
+    // You can perform any additional checks or operations here
+  } else {
+    // If there is no previous connection, connect to the database
+    mongoose
+      .connect(
+        process.env.MONGO as string,
+        {
+          useNewUrlParser: true,
+          useUnifiedTopology: true,
+        } as any
+      )
+      .then(() => {
+        console.log("Mongoose is now connected to the database");
+        // You can perform any additional operations after connecting here
+      })
+      .catch((err) => {
+        console.error("Error connecting to the database", err);
+      });
   }
 };
 
