e2994eb7c2c46852262100f541d7ef029f5a8844
Alexeykoh <Alexeykoh@icloud.com>
2024-01-26 16:02:35 +0300

    feat: update bg

--- gym-rat/src/app/layout.tsx
+++ gym-rat/src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   return (
     <AuthProvider>
       <html lang="en">
-        <body className={"text-white bg-zinc-800 bgImage object-contain"}>
+        <body className={"text-white bg-zinc-800"}>
           {children}
         </body>
       </html>
--- gym-rat/src/app/page.tsx
+++ gym-rat/src/app/page.tsx
@@ -1,10 +1,11 @@
 import connectMongoDB from "@/lib/mongodb";
 import Link from "next/link";
+import "./globals.css";
 
 export default function Home() {
   connectMongoDB();
   return (
-    <div className="flex flex-col gap-6 w-screen h-screen justify-center items-center">
+    <div className="flex flex-col gap-6 w-screen h-screen justify-center items-center bgImage object-contain backdrop-brightness-50">
       <Link
         className="bg-lime-400 p-3 mt-4 rounded-xl text-xl text-black w-fit text-center max-w-96 min-w-40"
         href={"/sign-in"}
