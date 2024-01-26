b8f50bf0a0c9248f65ae7b30baada4e12fa2892a
Alexeykoh <Alexeykoh@icloud.com>
2024-01-26 13:47:42 +0300

    feat: add log out button & user info

--- gym-rat/src/components/header/header.tsx
+++ gym-rat/src/components/header/header.tsx
@@ -1,15 +1,36 @@
 "use client";
+import { signOut, useSession } from "next-auth/react";
 import { FC } from "react";
-import Logo from "../logo/logo";
 
 type headerProps = {};
 
 const Header: FC<headerProps> = () => {
   //
-
+  const { data, status }: any = useSession();
+  const isAuth = status === "authenticated";
+  const userName = data?.user?.name;
+  // console.log(data);
+  //
   return (
-    <header className="flex justify-center items-center w-full fixed box-border p-4">
-      {/* <Nav /> */}
+    <header className="flex items-center box-border p-4 justify-between">
+      <div className="logo">GYM-Rat</div>
+      <div className="w-fit flex gap-8 items-center">
+        <div className="flex gap-2 items-center">
+          <div className="userIcon min-w-8 min-h-8 rounded-full bg-gray-600 flex items-center justify-center">
+            i
+          </div>
+          <p>{userName}</p>
+        </div>
+
+        <button
+          onClick={() => {
+            signOut();
+          }}
+          className="lg:hover:bg-gray-400 py-2 px-4 bg-gray-600 rounded-lg duration-150"
+        >
+          Log out
+        </button>
+      </div>
     </header>
   );
 };
