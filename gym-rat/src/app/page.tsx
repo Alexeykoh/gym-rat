b4f3d28ad22c005c96dda3194f866c79be91cc31
Alexeykoh <Alexeykoh@icloud.com>
2024-01-26 15:54:05 +0300

    refactor: change element position

--- gym-rat/src/app/page.tsx
+++ gym-rat/src/app/page.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function Home() {
   connectMongoDB();
   return (
-    <div className="flex flex-col gap-6 w-full justify-center items-center">
+    <div className="flex flex-col gap-6 w-screen h-screen justify-center items-center">
       <Link
         className="bg-lime-400 p-3 mt-4 rounded-xl text-xl text-black w-fit text-center max-w-96 min-w-40"
         href={"/sign-in"}
