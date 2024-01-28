4fab73518fffb786b90d6b88465531e7402ece92
Alexeykoh <alexeykoh@gmail.com>
2024-01-28 15:43:53 +0300

    feat: create redux test component

--- gym-rat/src/app/(private_route)/dashboard/page.tsx
+++ gym-rat/src/app/(private_route)/dashboard/page.tsx
@@ -1,3 +1,22 @@
+"use client";
+
+import {
+  decrement,
+  increment,
+  incrementByAmount,
+} from "@/app/GlobalRegux/Features/counter/counterSlice";
+import { useDispatch, useSelector } from "react-redux";
 export default function Dashboard() {
-  return <main className="">this is Dashboard</main>;
+  const count = useSelector((state: any) => state.counter.value);
+  const dispatch = useDispatch();
+  return (
+    <main className="">
+      <button onClick={() => dispatch(increment())}>Increment</button>
+      <span>{count}</span>
+      <button onClick={() => dispatch(decrement())}>Decrement</button>
+      <button onClick={() => dispatch(incrementByAmount(2))}>
+        Increment by 2
+      </button>
+    </main>
+  );
 }
