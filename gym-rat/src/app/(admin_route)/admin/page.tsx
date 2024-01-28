3ad1efe133419d32e8976aa80b6a34155656e2ed
Alexeykoh <alexeykoh@gmail.com>
2024-01-29 00:13:19 +0300

    feat: create admin page

---
+++ gym-rat/src/app/(admin_route)/admin/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function AdminPage() {
+  return (
+    <div className="w-full h-full flex flex-col gap-10 items-center justify-center">
+      this is admin page
+    </div>
+  );
+}
