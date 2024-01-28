8ce359f6bab2a56935d8dff109fd18b3172af44c
Alexeykoh <alexeykoh@gmail.com>
2024-01-28 15:44:24 +0300

    feat: add workout fetch data

--- gym-rat/src/app/(private_route)/workouts/page.tsx
+++ gym-rat/src/app/(private_route)/workouts/page.tsx
@@ -1,16 +1,60 @@
-import arm from "@/assets/arm.svg";
+"use client";
 import CardLayout from "@/components/cardLayout/cardLayout";
+
+import WorkoutForm from "@/components/forms/workout/WorkoutForm";
+import Modal from "@/components/widgets/modal/Modal";
 import WorkoutCard from "@/components/workoutCard/workoutCard";
-import { FC } from "react";
+import { useSession } from "next-auth/react";
+import { FC, useEffect, useState } from "react";
+import arm from "../../../../public/icons/arm.svg";
 
 type pageProps = {};
 
 const Workout: FC<pageProps> = () => {
+  const { data, status }: any = useSession();
+  //
+  const [openModal, setOpenModal] = useState<boolean>(false);
+  const [workouts, setWorkouts] = useState([]);
+  const [isLoading, setLoading] = useState(true);
+
+  function getWorkouts() {
+    setLoading(true);
+    fetch("/api/workout")
+      .then((res) => res.json())
+      .then((data) => {
+        setWorkouts(data?.message);
+        setLoading(false);
+      });
+  }
+
+  useEffect(() => {
+    getWorkouts();
+  }, []);
+  //
   return (
     <div className="flex flex-col gap-4 w-full pb-16">
-      <div className="px-4 py-2 fixed bottom-4 left-1/2 -translate-x-1/2 bg-lime-500 rounded-2xl z-20">
+      {openModal ? (
+        <Modal
+          close={() => {
+            setOpenModal(false);
+          }}
+        >
+          <WorkoutForm
+            modalAction={() => {
+              setOpenModal(false);
+            }}
+            getWorkouts={getWorkouts}
+          />
+        </Modal>
+      ) : null}
+      <button
+        onClick={() => {
+          setOpenModal(true);
+        }}
+        className="px-4 py-2 fixed bottom-4 left-1/2 -translate-x-1/2 bg-lime-500 rounded-2xl z-20 text-black shadow-lg"
+      >
         add workout
-      </div>
+      </button>
       <form className="flex items-center pb-4 max-w-96 w-full self-center">
         <label htmlFor="simple-search" className="sr-only">
           Search
@@ -30,68 +74,46 @@ const Workout: FC<pageProps> = () => {
         >
           <svg
             className="w-4 h-4"
-            aria-hidden="true"
+            // aria-hidden="true"
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 20 20"
           >
             <path
               stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
               d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
             />
           </svg>
           <span className="sr-only">Search</span>
         </button>
       </form>
-      <div className="flex flex-wrap gap-4">
-        <CardLayout>
-          <WorkoutCard
-            icon={arm}
-            title={"Круговая тренировка"}
-            description={"Тренировка для восстановления после перерыва"}
-            date={"18.01.2024"}
-            id={"0"}
-          />
-        </CardLayout>
-        <CardLayout>
-          <WorkoutCard
-            icon={arm}
-            title={"Круговая тренировка"}
-            description={"Тренировка для восстановления после перерыва"}
-            date={"20.01.2024"}
-            id={"1"}
-          />
-        </CardLayout>
-        <CardLayout>
-          <WorkoutCard
-            icon={arm}
-            title={"Грудь/Бицепс"}
-            description={"Акцент на грудь. Установка рекордного веса"}
-            date={"23.01.2024"}
-            id={"2"}
-          />
-        </CardLayout>
-        <CardLayout>
-          <WorkoutCard
-            icon={arm}
-            title={"Грудь/Бицепс"}
-            description={"Акцент на грудь. Установка рекордного веса"}
-            date={"23.01.2024"}
-            id={"3"}
-          />
-        </CardLayout>
-        <CardLayout>
-          <WorkoutCard
-            icon={arm}
-            title={"Грудь/Бицепс"}
-            description={"Акцент на грудь. Установка рекордного веса"}
-            date={"23.01.2024"}
-            id={"4"}
-          />
-        </CardLayout>
+      <div className="grid grid-cols-1 lg:grid-cols-4 gap-4">
+        {!workouts ? (
+          <>
+            <p>Loading...</p>
+          </>
+        ) : (
+          <>
+            {workouts.map(({ date, description, name, _id }, ind) => {
+              const workoutDate = new Date(date);
+              // console.log(workoutDate.toLocaleString());
+              return (
+                <CardLayout key={ind}>
+                  <WorkoutCard
+                    icon={arm}
+                    title={name}
+                    description={description}
+                    date={date}
+                    id={_id}
+                  />
+                </CardLayout>
+              );
+            })}
+          </>
+        )}
       </div>
     </div>
   );
